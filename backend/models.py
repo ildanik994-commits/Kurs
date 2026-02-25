b3c7f93e14de007ebf020e4f8357b5396a3998af
Ildan <ildanik994@gmail.com>
2026-02-26 02:49:13 +0300

    Fix relative import in models

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, DateTime, Boolean, Text
 from sqlalchemy.orm import relationship
 import datetime
-from database import Base
+from .database import Base
 
 # === Tables for Lookups (Dictionaries) ===
 
